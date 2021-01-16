c0f375110054d722fadb3b840bffdbece986ebc2
jigsaw <m@jgs.me>
2021-01-16 15:24:48 +0900

    ISR

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,32 @@
-import {useMemo} from 'react'
+import {useState, useEffect} from 'react'
 import Head from 'next/head'
 
 const endpoint = process.env.VERCEL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
 
-export async function getServerSideProps () {
+export async function getStaticPaths () {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export async function getStaticProps () {
   const res = await fetch(`${endpoint}/api/latest`)
   const status = await res.json()
   return {
     props: {
-      status
-    }
+      status,
+      generatedAt: new Date().getTime()
+    },
+    revalidate:60
   }
 }
 
-export default function TopPage ({ status }) {
-  const createdAt = useMemo(() => {
-    if (status) {
-      return new Date(status.created_at).toLocaleString()
-    } else {
-      return ''
-    }
-  }, [status])
+export default function TopPage ({ status, generatedAt }) {
+  const [createdAt, setCreatedAt] = useState('')
+  useEffect(() => {
+    setCreatedAt(new Date(status.created_at).toLocaleString())
+  }, [])
   return status ? (
     <>
       <Head>
@@ -29,6 +35,7 @@ export default function TopPage ({ status }) {
       <div className="h-screen flex flex-col justify-center">
         <p className="text-9xl ml-8">{status.text}</p>
         <p className="text-2xl ml-16">{createdAt}</p>
+        <p className="text-2xl ml-16">{generatedAt}</p>
       </div>
     </>
   ) : null
